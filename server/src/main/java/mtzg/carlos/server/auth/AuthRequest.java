38b91a03732d10f4528aab8d1c3203a1b7d88e92
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-11-30 02:27:33 -0600

    feat: Add validation annotations for email and password in AuthRequest

--- server/src/main/java/mtzg/carlos/server/auth/AuthRequest.java
+++ server/src/main/java/mtzg/carlos/server/auth/AuthRequest.java
@@ -1,5 +1,7 @@
 package mtzg.carlos.server.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,9 +17,12 @@ public class AuthRequest {
     public static final String NO_ANGLE_BRACKETS_MESSAGE = "must not contain angle brackets";
     public static final String NO_ANGLE_BRACKETS_REGEX = "^[^<>]*$";
 
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     @Pattern(regexp = NO_ANGLE_BRACKETS_REGEX, message = NO_ANGLE_BRACKETS_MESSAGE)
     private String email;
 
+    @NotBlank(message = "Password is required")
     @Pattern(regexp = NO_ANGLE_BRACKETS_REGEX, message = NO_ANGLE_BRACKETS_MESSAGE)
     private String password;
 }

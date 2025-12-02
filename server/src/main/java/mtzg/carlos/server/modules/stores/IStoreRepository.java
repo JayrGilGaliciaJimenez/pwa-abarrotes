8734b0c7b93145007199f2a7ad1f9d97061326c9
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-12-02 11:56:36 -0600

    fix: correct return type of findByUuid method in IStoreRepository to Optional<StoreModel>

--- server/src/main/java/mtzg/carlos/server/modules/stores/IStoreRepository.java
+++ server/src/main/java/mtzg/carlos/server/modules/stores/IStoreRepository.java
@@ -6,12 +6,10 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import mtzg.carlos.server.modules.products.ProductModel;
-
 @Repository
 public interface IStoreRepository extends JpaRepository<StoreModel, Long> {
 
-    Optional<ProductModel> findByUuid(UUID uuid);
+    Optional<StoreModel> findByUuid(UUID uuid);
 
     Optional<StoreModel> findByNameIgnoreCase(String name);
 }

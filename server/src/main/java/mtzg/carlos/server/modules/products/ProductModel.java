a20198c4299d282f93ebbce982401a0a815697f5
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-12-02 00:07:23 -0600

    fix: ensure UUID column is unique in ProductModel

--- server/src/main/java/mtzg/carlos/server/modules/products/ProductModel.java
+++ server/src/main/java/mtzg/carlos/server/modules/products/ProductModel.java
@@ -39,7 +39,7 @@ public class ProductModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "uuid", nullable = false)
+    @Column(name = "uuid", nullable = false, unique = true)
     private UUID uuid;
 
     @Column(name = "name", nullable = false)

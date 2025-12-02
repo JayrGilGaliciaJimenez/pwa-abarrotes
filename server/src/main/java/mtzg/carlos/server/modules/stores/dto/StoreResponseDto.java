1dd3b8cf7a9d39e22c520b618df997d135203e5e
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-12-02 11:38:51 -0600

    feat: add StoreResponseDto class for store response data transfer

---
+++ server/src/main/java/mtzg/carlos/server/modules/stores/dto/StoreResponseDto.java
@@ -0,0 +1,23 @@
+package mtzg.carlos.server.modules.stores.dto;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class StoreResponseDto {
+
+    private UUID uuid;
+    private String name;
+    private String address;
+    private Double latitude;
+    private Double longitude;
+}

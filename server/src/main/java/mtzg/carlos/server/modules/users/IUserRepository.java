8897418356253a1a8c0b03a4cb4d728ed805acb6
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-12-03 14:31:36 -0600

    feat: add method to find UserModel by UUID in IUserRepository

--- server/src/main/java/mtzg/carlos/server/modules/users/IUserRepository.java
+++ server/src/main/java/mtzg/carlos/server/modules/users/IUserRepository.java
@@ -1,10 +1,13 @@
 package mtzg.carlos.server.modules.users;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IUserRepository extends JpaRepository<UserModel, Long> {
 
     Optional<UserModel> findByEmail(String email);
+
+    Optional<UserModel> findByUuid(UUID uuid);
 }

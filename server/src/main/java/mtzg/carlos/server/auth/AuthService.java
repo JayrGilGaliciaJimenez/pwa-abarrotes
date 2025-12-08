bf1af37cc6f255a46bca75f0b2d0faf750727c6c
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-12-07 23:47:27 -0600

    refactor: remove unused user registration method from AuthService

--- server/src/main/java/mtzg/carlos/server/auth/AuthService.java
+++ server/src/main/java/mtzg/carlos/server/auth/AuthService.java
@@ -1,22 +1,16 @@
 package mtzg.carlos.server.auth;
 
-import java.util.Optional;
-import java.util.UUID;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import mtzg.carlos.server.modules.jwt.JwtService;
 import mtzg.carlos.server.modules.users.IUserRepository;
-import mtzg.carlos.server.modules.users.Role;
-import mtzg.carlos.server.modules.users.UserModel;
-import mtzg.carlos.server.modules.users.dto.UserRegisterDto;
+
 import mtzg.carlos.server.utils.Utilities;
 
 @Service
@@ -24,26 +18,8 @@ import mtzg.carlos.server.utils.Utilities;
 public class AuthService {
 
         private final IUserRepository userRepository;
-        private final PasswordEncoder passwordEncoder;
         private final JwtService jwtService;
         private final AuthenticationManager authenticationManager;
-
-        public ResponseEntity<Object> register(UserRegisterDto request) {
-                Optional<UserModel> existingUser = userRepository.findByEmail(request.getEmail());
-                if (existingUser.isPresent()) {
-                        return Utilities.simpleResponse(HttpStatus.CONFLICT, "Unable to complete registration");
-                }
-                var user = UserModel.builder()
-                                .uuid(UUID.randomUUID())
-                                .name(request.getName())
-                                .email(request.getEmail())
-                                .password(passwordEncoder.encode(request.getPassword()))
-                                .role(Role.USER)
-                                .build();
-                userRepository.save(user);
-                var jwtToken = jwtService.generateToken(user);
-                return Utilities.authResponse(HttpStatus.OK, "User registered successfully", jwtToken);
-        }
 
         public ResponseEntity<Object> authenticate(AuthRequest request) {
                 authenticationManager.authenticate(
